fb0a5925971454c8e6c0d02a7c1ecadbea7eea8b
Jorge Rivera <jorgeulisesrivera.a@gmail.com>
2020-11-04 11:40:58 -0300

    add favicon

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,27 @@
-self.addEventListener("install", function (event) {
+/*self.addEventListener("install", function (event) {
     console.log("Hello world from the Service Worker 🤙");
+});*/
+
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
