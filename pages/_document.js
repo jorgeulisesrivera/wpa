85ec1a0daadd840b69ceffe6dd8795037b9bcf77
Jorge Rivera <jorgeulisesrivera.a@gmail.com>
2020-11-06 00:37:38 -0300

    fix url images news

--- pages/_document.js
+++ pages/_document.js
@@ -14,13 +14,13 @@ class MyDocument extends Document {
         <title>Hello World</title>
         <link rel="manifest" href="/manifest.json"/>
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="images/hello-icon-152.png"/>
+        <link rel="apple-touch-icon" href="images/apple-icon-152x152.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="theme-color" content="white"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
         <meta name="apple-mobile-web-app-title" content="Hello World"/>
-        <meta name="msapplication-TileImage" content="images/hello-icon-144.png"/>
+        <meta name="msapplication-TileImage" content="images/android-icon-144x144.png"/>
         <meta name="msapplication-TileColor" content="#FFFFFF"/>
         </Head>
         <body>
